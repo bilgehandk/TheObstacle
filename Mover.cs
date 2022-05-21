6966f1088ec399c3ebd78dec378b65c583144d44
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2022-05-21 20:48:05 +0300

    wip

--- Mover.cs
+++ Mover.cs
@@ -2,25 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Mover : MonoBehaviour
 {
-    
-    [SerializeField] private float moveSpeed = 10f;
 
+    [SerializeField] private float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+    
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome the the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal")* Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         
         transform.Translate(xValue,0,zValue) ;
     }
+    
 }
  
